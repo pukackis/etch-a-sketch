472d017f15a41b10c46536208dd6e4aaf830c42f
Devis <104310438+Deviss12@users.noreply.github.com>
2023-08-12 01:34:55 +0200

    add coloring on hover function, start work on functionality of the website

--- script.js
+++ script.js
@@ -2,17 +2,33 @@
 
 const sketchContainer = document.querySelector(".sketch_container");
 
-function generateGrid(size) {
-  for (let i = 0; i < size; i++) {
-    // fills the remaining columns
-    for (let j = 0; j < size; j++) {
-      let gridSquare = document.createElement("div");
-      gridSquare.classList.add("gridsquare");
-      gridSquare.style.height = `${960 / size}px`; // Set height using style property
-      gridSquare.style.width = `${960 / size}px`; // Set width using style property
-      sketchContainer.appendChild(gridSquare);
-    } // generates columns in one dimension
+let size = prompt("please select grid size");
+
+function generateGrid(value) {
+  if (value <= 100) {
+    for (let i = 0; i < value; i++) {
+      // fills the remaining columns
+      for (let j = 0; j < value; j++) {
+        let gridSquare = document.createElement("div");
+        gridSquare.classList.add("gridsquare");
+        gridSquare.style.height = `${960 / value}px`; // Set height using style property
+        gridSquare.style.width = `${960 / value}px`; // Set width using style property
+        sketchContainer.appendChild(gridSquare);
+      } // generates columns in one dimension
+    }
+  } else {
+    alert("Invalid input, please use numbers between 1 and 100");
   }
 }
+generateGrid(size);
 
 // generateGrid function is responsible for grid generation, used nested loops to ensure that rows and columns are filled exactly as intended
+
+//Add an event listener for the 'mouseover' event on the sketch container
+sketchContainer.addEventListener("mouseover", function (event) {
+  //Check if the element that triggered the event has the gridsquare class
+  if (event.target.classList.contains("gridsquare")) {
+    //Change the background color of a single square to black
+    event.target.style.backgroundColor = "black";
+  }
+});
