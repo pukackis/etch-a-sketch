5fa26afea18a1c0178e33447f9b119908c67ce97
Devis <104310438+Deviss12@users.noreply.github.com>
2023-08-14 15:36:43 +0200

    Add toggle rgb button to switch between drawing modes: black or rgb

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
-let grid = false;
+let grid = true;
+let rgb = false;
 
 const sketchContainer = document.querySelector(".sketch_container");
 
@@ -12,6 +13,10 @@ const toggleGridButton = document
   .querySelector("#toggle_grid")
   .addEventListener("click", toggleGrid);
 
+const toggleRGBButton = document
+  .querySelector("#toggle_rgb")
+  .addEventListener("click", toggleRGB);
+
 function buttonPrompt() {
   //Clears sketching area before starting
   sketchContainer.replaceChildren();
@@ -22,6 +27,8 @@ function buttonPrompt() {
 }
 
 function generateGrid(value) {
+  grid = true;
+  rgb = false;
   if (value <= 100) {
     // fills the remaining columns
     for (let i = 0; i < value; i++) {
@@ -41,11 +48,19 @@ function generateGrid(value) {
 
 document.addEventListener("mousedown", () => {
   clearSelection();
-  sketchContainer.addEventListener("mouseover", colorBlack);
+  if (rgb === false) {
+    sketchContainer.addEventListener("mouseover", colorBlack);
+  } else {
+    sketchContainer.addEventListener("mouseover", colorRGB);
+  }
 });
 
 document.addEventListener("mouseup", () => {
-  sketchContainer.removeEventListener("mouseover", colorBlack);
+  if (rgb === false) {
+    sketchContainer.removeEventListener("mouseover", colorBlack);
+  } else {
+    sketchContainer.removeEventListener("mouseover", colorRGB);
+  }
 });
 
 function clearSelection() {
@@ -64,16 +79,33 @@ function colorBlack(event) {
   }
 }
 
+function colorRGB(event) {
+  //Check if the element that triggered the event has the gridsquare class
+  if (event.target.classList.contains("gridsquare")) {
+    //Change the background color of a single square to black
+    event.target.style.backgroundColor = `rgb(
+      ${Math.random() * 256},
+      ${Math.random() * 256},
+      ${Math.random() * 256}
+    )`;
+  }
+}
+
+function toggleRGB() {
+  rgb = !rgb;
+}
+
+// Function to toggle grid on or off
 function toggleGrid() {
-  const gridSquares = document.querySelectorAll(".sketch_container > div");
+  const gridSquares = document.querySelectorAll(".sketch_container > div"); // select all square divs that were created in sketch_container upon starting the game
 
   gridSquares.forEach((square) => {
+    // change outline for each div depending wheter grid is true or false
     if (grid) {
       square.style.outline = "solid 1px rgba(204, 204, 204, 0.226)";
     } else {
       square.style.outline = "none";
     }
   });
-  grid = !grid;
-  console.log("toggle clicked");
+  grid = !grid; // change boolean value of grid on button press
 }
